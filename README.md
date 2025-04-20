de47771499f6adf385f6731a1f5810810c165950
parikxxit <singhp154154@gmail.com>
2025-04-20 17:34:32 +0530

    added logic to redicect to default page when started scrolling

---
+++ README.md
@@ -0,0 +1,38 @@
+# No Shorts No Reels (suggent a better name :)
+## TLDR; you can still use youtube and insta but can escape doom scrolling 
+
+## trying to solve my infinite scrolling problem in my browser (I tried uninstalling native apps but now I am hooked on browser and I can't fully disable social media coz i watch lot of yt tutorial so i created this)
+
+A Chrome extension that blocks YouTube Shorts and Instagram Reels to help you stay focused and avoid endless scrolling.
+
+## Features
+
+- Blocks YouTube Shorts from appearing in your feed
+- Removes the Shorts tab from YouTube navigation
+- Blocks Instagram Reels from appearing in your feed
+- Removes the Reels tab from Instagram navigation
+- Redirects you away from Shorts/Reels URLs
+
+## Installation
+
+1. Download or clone this repository
+2. Open Chrome and go to `chrome://extensions/`
+3. Enable "Developer mode" in the top right corner
+4. Click "Load unpacked" and select the directory containing the extension files
+5. The extension should now be installed and active
+
+## Usage
+
+Once installed, the extension will automatically:
+- Block YouTube Shorts from appearing in your feed
+- Block Instagram Reels from appearing in your feed
+- Remove Shorts/Reels navigation elements
+- Redirect you away from Shorts/Reels URLs (NEED TO PROVIDE SUPPORT FOR SOME STATIC DISAPLY OVER REDIRECTION TO INCREASE FRICTION)
+No configuration is needed - just install and forget!
+
+Upcoming Features:
+- Block twitter videos
+
+## Contributing
+
+Feel free to submit issues and enhancement requests! :) 
---
+++ content.js
@@ -0,0 +1,55 @@
+function blockYouTubeShorts() {
+    const shortsLink = document.querySelector('a[href="/shorts"]');
+    if (shortsLink) {
+        shortsLink.parentElement.remove();
+    }
+
+    const shortsElements = document.querySelectorAll('ytd-rich-item-renderer, ytd-reel-item-renderer');
+    shortsElements.forEach(element => {
+        if (element.querySelector('a[href*="/shorts/"]')) {
+            element.remove();
+        }
+    });
+
+    // TODO: Rather then redirecting GIVE SOME STATIC Image that reminds to stop maybe something motivational
+    if (window.location.pathname.startsWith('/shorts/')) {
+        window.location.href = 'https://www.youtube.com';
+    }
+}
+
+function blockInstagramReels() {
+    // Remove Reels from the navigation
+    const reelsLink = document.querySelector('a[href="/reels/"]');
+    if (reelsLink) {
+        reelsLink.parentElement.remove();
+    }
+
+    // Block Reels from appearing in the feed
+    const reelsElements = document.querySelectorAll('article');
+    reelsElements.forEach(element => {
+        if (element.querySelector('a[href*="/reel/"]')) {
+            element.remove();
+        }
+    });
+
+    // TODO: Rather then redirecting GIVE SOME STATIC Image that reminds to stop maybe something motivational
+    if (window.location.pathname.startsWith('/reels/') || window.location.pathname.startsWith('/reel/')) {
+        window.location.href = 'https://www.instagram.com';
+    }
+}
+
+function initializeBlocking() {
+    if (window.location.hostname.includes('youtube.com')) {
+        blockYouTubeShorts();
+        // Set up a mutation observer to continuously block new Shorts
+        const observer = new MutationObserver(blockYouTubeShorts);
+        observer.observe(document.body, { childList: true, subtree: true });
+    } else if (window.location.hostname.includes('instagram.com')) {
+        blockInstagramReels();
+        // Set up a mutation observer to continuously block new Reels
+        const observer = new MutationObserver(blockInstagramReels);
+        observer.observe(document.body, { childList: true, subtree: true });
+    }
+}
+
+initializeBlocking(); 
